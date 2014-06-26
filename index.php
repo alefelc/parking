b5ab343dc77a36108334640a99c8d11678d23e80
alefelc <HOME@Alejandros-MacBook-Pro.local>
2014-06-26 13:24:02 -0300

    1.1

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 ﻿<!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Berazategui Estacionamiento Responsable</title>
-    <meta name="description" content="Your description here" />
-    <meta name="keywords" content="Your keywords here" />
-    <meta name="author" content="Your author name here" />
-    <!-- Mobile specific -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Apple Touch Icons -->
-    <link rel="apple-touch-icon" sizes="144x144" href="images/icons/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-touch-icon-57-precomposed.png">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="images/icons/favicon.png">
-    <!-- Google fonts -->
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
-    <!-- Stylesheets -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/owl.carousel.css" rel="stylesheet">
-    <link href="css/global.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Berazategui Estacionamiento Responsable</title>
+<meta name="description" content="Your description here"/>
+<meta name="keywords" content="Your keywords here"/>
+<meta name="author" content="Your author name here"/>
+<!-- Mobile specific -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Apple Touch Icons -->
+<link rel="apple-touch-icon" sizes="144x144" href="images/icons/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-touch-icon-57-precomposed.png">
+<!-- Favicon -->
+<link rel="shortcut icon" href="images/icons/favicon.png">
+<!-- Google fonts -->
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="css/owl.carousel.css" rel="stylesheet">
+<link href="css/global.css" rel="stylesheet">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="http://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
-    <script type="text/javascript" src="js/gmap3.min.js"></script>
-    <style>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
+<script type="text/javascript" src="js/gmap3.min.js"></script>
+<style>
     body {
         text-align:center;
     }
@@ -42,8 +42,8 @@
         width: 100%;
         height: 500px;
     }
-    </style>
-    <script>
+</style>
+<script>
         function login() {
             if (!$("#dni").val()) {
                 alert("completar dni");
@@ -90,7 +90,7 @@
             alert("Login incorrecto");
         }
     </script>
-    <script type="text/javascript">
+<script type="text/javascript">
     $(function() {
         $('#test1').gmap3({
             map: {
@@ -152,148 +152,181 @@
     });
     </script>
 </head>
-
 <body>
-    <!-- Begin preloader -->
-    <div id="preloader">
-        <div id="status">
-            <div id="circleG">
-                <div id="circleG_1" class="circleG"></div>
-                <div id="circleG_2" class="circleG"></div>
-                <div id="circleG_3" class="circleG"></div>
+<!-- Begin preloader -->
+<div id="preloader">
+    <div id="status">
+        <div id="circleG">
+            <div id="circleG_1" class="circleG">
+            </div>
+            <div id="circleG_2" class="circleG">
+            </div>
+            <div id="circleG_3" class="circleG">
             </div>
         </div>
     </div>
-    <!-- End preloader -->
-    <div id="wrap">
-        <div id="content-area">
-            <!-- Begin charge section -->
-            <section class="panel-countdown">
-                <div id="countdown_dashboard">
-                    <div class="container">
+</div>
+<!-- End preloader -->
+<div id="wrap">
+    <div id="content-area">
+        <!-- Begin charge section -->
+        <section class="panel-countdown">
+        <div id="countdown_dashboard">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
                         <div class="row">
-                            <div class="col-xs-12 col-sm-6">
-                                <div class="row">
-                                    <div class="col-xs-12 col-md-8">
-                                        <img src="images/logo.png" class="img-responsive">
+                            <div class="col-xs-12 col-md-8">
+                                <img src="images/logo.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6" id="divSaldoOrLogin">
+                        <section>
+                        <div class="social-btns full">
+                            <button type="button" class="btn btn-primary btn-lg">Todavía no estás inscripto?</button>
+                            <p class="legend">
+                                 Descargá la planilla para registrarte
+                            </p>
+                        </div>
+                        <div class="tagline">
+                        </div>
+                        </section>
+                        <section>
+                        <form action="#" method="get" accept-charset="utf-8" class="form-horizontal" role="form">                            
+                                <div class="form-group has-feedback">
+                                    <input type="text" class="form-control" id="dni" name="dni" placeholder="D.N.I." required="">
+                                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                                </div>
+                                <div class="form-group has-feedback">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña" required="">
+                                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-primary forget" data-toggle="modal" data-target=".bs-example-modal-sm">Olvidaste la contraseña?</button>
+                                    <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-sm">
+                                            <div class="modal-content">
+                                                <form>
+                                                    <div class="form-group has-feedback">
+                                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Tu Email" required="">
+                                                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                                                    </div>
+                                                    <p class="legend">
+                                                         Te enviaremos un Email con tu contraseña
+                                                    </p>
+                                                    </br>
+                                                    <button type="button" onclick="javascript:login()" class="btn btn-default">Enviar</button>
+                                                </form>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-xs-12 col-sm-6" id="divSaldoOrLogin">
-                                <div class="row subscribe">
-                                    <div class=".col-xs-6">
-                                        <div id="content">
-                                            <h2 class="title">Saldo Actual</h2>
-                                            <h4 class="title">$ 300</h4>
-                                            <h2 class="title" style="margin-top:30px">Recarga Saldo</h2>
-                                        </div>
-                                        <form action="#" method="post">
-                                            <input type="email" name="Email" required="" class="subscribe-box" placeholder="Ingresa el PIN">
-                                            <input type="submit" value="Cargar" name="subscribe" class="subscribe-btn">
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-default">Entrar</button>
+                                </div>                            
+                        </form>
+                        </section>
                     </div>
                 </div>
-            </section>
-            <!-- End charge section -->
-            <!-- Begin sale's point section -->
-            <section class="panel-about">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div id="test1" class="gmap3"></div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- End sale's point section -->
-            <!-- Begin parking section -->
-            <section class="panel-services">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- End parking section -->
-            <!-- Begin contact section -->
-            <section class="panel-contact">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-6">
-                            <div class="row">
-                                <div class="col-xs-12 col-md-8">
-                                    <img src="images/logo.png" class="img-responsive">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-6">
-                            <div class="row subscribe">
-                                <div class=".col-xs-6">
-                                    <div id="content">
-                                        <h1 class="title">Contacto</h1>
-                                    </div>
-                                    <form role="form" action="#" method="post">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Nombre" required="">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" required="">
-                                        </div>
-                                        <div class="form-group">
-                                            <textarea class="form-control" rows="3" placeholder="Mensaje" required=""></textarea>
-                                        </div>
-                                        <button type="submit" class="btn btn-default subscribe-btn">Enviar</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- End contact section -->
+            </div>
         </div>
+        </section>
+        <!-- End charge section -->
+        <!-- Begin sale's point section -->
+        <section class="panel-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div id="test1" class="gmap3">
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        <!-- End sale's point section -->
+        <!-- Begin parking section -->
+        <section class="panel-services">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                </div>
+            </div>
+        </div>
+        </section>
+        <!-- End parking section -->
+        <!-- Begin contact section -->
+        <section class="panel-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-8">
+                            <img src="images/logo.png" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="row subscribe">
+                        <div class=".col-xs-6">
+                            <div id="content">
+                                <h1 class="title">Contacto</h1>
+                            </div>
+                            <form role="form" action="#" method="post">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Nombre" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" required="">
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="3" placeholder="Mensaje" required=""></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-default subscribe-btn">Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        <!-- End contact section -->
     </div>
-    <!-- Begin tabs section -->
-    <div class="tab-container">
-        <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab active">
-            <span class="tab-detail">
-                <span class="tab-heading">Saldo</span>
-                <i class="fa fa-usd"></i>
-            </span>
-        </a>
-        <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab">
-            <span class="tab-detail">
-                <span class="tab-heading">Puntos de Venta</span>
-                <i class="fa fa-home"></i>
-            </span>
-        </a>
-        <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab">
-            <span class="tab-detail">
-                <span class="tab-heading">Estacionamientos</span>
-                <i class="fa fa-road"></i>
-            </span>
-        </a>
-        <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab">
-            <span class="tab-detail">
-                <span class="tab-heading">Contacto</span>
-                <i class="fa fa-envelope"></i>
-            </span>
-        </a>
-    </div>
-    <!-- En tabs section -->
-    <!-- Scripts -->
-    <script src="js/jquery.backstretch.min.js"></script>
-    <script src="js/jquery.lwtCountdown-1.0.js"></script>
-    <script src="js/retina-1.1.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/global.js"></script>
+</div>
+<!-- Begin tabs section -->
+<div class="tab-container">
+    <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab active">
+    <span class="tab-detail">
+    <span class="tab-heading">Saldo</span>
+    <i class="fa fa-usd"></i>
+    </span>
+    </a>
+    <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab">
+    <span class="tab-detail">
+    <span class="tab-heading">Puntos de Venta</span>
+    <i class="fa fa-home"></i>
+    </span>
+    </a>
+    <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab">
+    <span class="tab-detail">
+    <span class="tab-heading">Estacionamientos</span>
+    <i class="fa fa-road"></i>
+    </span>
+    </a>
+    <a href="javascript:void(0)" class="col-xs-6 col-sm-3 tab">
+    <span class="tab-detail">
+    <span class="tab-heading">Contacto</span>
+    <i class="fa fa-envelope"></i>
+    </span>
+    </a>
+</div>
+<!-- En tabs section -->
+<!-- Scripts -->
+<script src="js/jquery.backstretch.min.js"></script>
+<script src="js/jquery.lwtCountdown-1.0.js"></script>
+<script src="js/retina-1.1.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/global.js"></script>
 </body>
-
 </html>
